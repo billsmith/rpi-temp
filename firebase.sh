1185be524b0d0622b6183ca1112ba0455b0e7ac6
Bill Smith <billsmithaustin@yahoo.com>
2017-09-01 06:10:56 -0500

    use auth token

--- firebase.sh
+++ firebase.sh
@@ -7,5 +7,5 @@ currenttime=$(date +%s)
 T=$(echo "scale=1; $(cat /sys/bus/w1/devices/${DEVICE}/w1_slave|tail -1|cut -d= -f2) / 1000.0 * 1.8 + 32" | bc -l)
 
 curl  -X PUT -d ${T} \
-https://devops-temperature.firebaseio.com/rest/$(date +%s).json
+https://devops-temperature.firebaseio.com/rest/$(date +%s).json?auth=${AUTH}
 
